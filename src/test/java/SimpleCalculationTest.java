ed7bb0ca7417f0b2aed266476e627f080454a2f1
RimshaArif <rimshaarif723@gmail.com>
2022-06-20 00:48:02 +0500

    Changes

--- src/test/java/SimpleCalculationTest.java
+++ src/test/java/SimpleCalculationTest.java
@@ -2,7 +2,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.core.IsEqual.equalTo;
-import static org.junit.Assert.assertThat;
+import org.hamcrest.MatcherAssert;
 
 public class SimpleCalculationTest {
     private SimpleCalculation simpleCalculation;
@@ -16,13 +16,27 @@ public class SimpleCalculationTest {
     public void multiply() {
         int result = simpleCalculation.multiply(2, 4);
 
-        assertThat(result, equalTo(8));
+        MatcherAssert.assertThat(result, equalTo(8));
+    }
+
+    @Test
+    public void divide() {
+        int result = simpleCalculation.divide(4, 2);
+
+        MatcherAssert.assertThat(result, equalTo(2));
+    }
+
+    @Test
+    public void add() {
+        int result = simpleCalculation.add(4, 2);
+
+        MatcherAssert.assertThat(result, equalTo(6));
     }
 
     @Test
     public void subtract() {
         int result = simpleCalculation.subtract(4, 2);
 
-        assertThat(result, equalTo(2));
+        MatcherAssert.assertThat(result, equalTo(2));
     }
 }
